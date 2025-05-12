a1ae30a03963d821ae5f99165855e6012f864682
duraimuruganezhumalai <76684739+duraimuruganezhumalai@users.noreply.github.com>
2025-05-12 17:38:10 +0530

    Add files via upload

---
+++ javascript-advice-slip-app/adviceslip.js
@@ -0,0 +1,29 @@
+const appendAdviceSlip = document.querySelector(".advice-slip-text");
+const adviceSlipBtn = document.querySelector(".advice-slip-btn");
+const loader = document.querySelector("#overlay");
+
+
+const getAdviceSlipData = async () => {
+
+    try {
+        loader.style.display = "block"
+        appendAdviceSlip.textContent = " ";
+
+        const res = await fetch('https://api.adviceslip.com/advice')
+        const data = await res.json();
+
+        loader.style.display = "none"
+        console.log(data);
+        appendAdviceSlip.textContent = `${data.slip.advice}`
+
+    } catch (error) {
+        loader.style.display = "none"
+        appendAdviceSlip.textContent = `Advice slip is not found ${error}`
+    }
+}
+
+//button click event
+adviceSlipBtn.addEventListener('click', getAdviceSlipData)
+
+//initial loading function
+getAdviceSlipData();
